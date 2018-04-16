7bece22f22c6ca4ef54dc8b523feab7d6118437c
MichelBecerra <michelandreabc@gmail.com>
2018-04-16 01:04:14 -0500

    adding scrollview

--- Screens/MateriasScreen.js
+++ Screens/MateriasScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Alert, AppRegistry, TouchableOpacity, StyleSheet, Text, View } from 'react-native';
+import { Alert, AppRegistry, TouchableOpacity, StyleSheet, ScrollView, Text, View } from 'react-native';
 import {StackNavigator} from 'react-navigation';
 import { Table, Row, Rows } from 'react-native-table-component';
 
@@ -12,14 +12,18 @@ export default class MateriasScreen extends Component {
           <Text>Log Out</Text>
       </TouchableOpacity>
   };
+  componentDidMount(){
+    this.getMaterias().done();
+  }
   constructor(props) {
     super(props)
     this.state = {
+      materias_data: {materias: 'ninguna'},
       user_data: this.props.navigation.state.params.user_data,
       student_data: this.props.navigation.state.params.student_data,
-      tableHead: ['Head', 'Head2', 'Head3', 'Head4'],
+      tableHead: ['Clave', 'Semestre', 'Nombre', 'Requiere'],
       tableData: [
-        ['1', '2', '3', '4'],
+        ['1', '2', '3', '4', ],
         ['a', 'b', 'c', 'd'],
         ['1', '2', '3', '456\n789'],
         ['a', 'b', 'c', 'd']
@@ -30,11 +34,32 @@ export default class MateriasScreen extends Component {
     const { navigate } = this.props.navigation;
     const { params } = this.props.navigation.state;
     const state = this.state;
+    const styles = StyleSheet.create({
+      container: { flex: 1, padding: 16, paddingTop: 30, backgroundColor: '#fff' },
+      head: { height: 40, backgroundColor: '#f1f8ff' },
+      text: { margin: 6 },
+      textContainer: {
+        margin: 20
+      },
+      btn: {
+        padding: 15,
+        width: "66%",
+        margin: 70,
+        borderWidth: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#841584',
+      },
+      txt: {
+        color: '#C0C0C0',
+      },
+    })
     return (
+      <ScrollView>
       <View style={styles.container}>
         <Text>Lista de materias</Text>
         <Text>matricula = {student_data.matricula}</Text>
-        <Text>json = {this.getMaterias}</Text>
+        <Text>json = {JSON.stringify(this.state.materias_data)}</Text>
         <Table borderStyle={{borderWidth: 2, borderColor: '#c8e1ff'}}>
           <Row data={state.tableHead} style={styles.head} textStyle={styles.text}/>
           <Rows data={state.tableData} textStyle={styles.text}/>
@@ -45,40 +70,24 @@ export default class MateriasScreen extends Component {
             student_data: this.state.student_data})} style={styles.btn}>
             <Text style={styles.txt}>Ver grupos</Text>
           </TouchableOpacity>
-        </View>
+        </View>       
       </View>
+      </ScrollView>
     );
   }
-}
-
-function getMaterias() {
-  return fetch('http://sis-operativos-2018.herokuapp.com/materias.php')
+  
+  getMaterias = async() => {
+    fetch('http://sis-operativos-2018.herokuapp.com/materias.php', {
+      method: 'GET',
+      headers: {'Accept' : "application/json", 'Content-Type' : 'application/json',},
+    })
     .then((response) => response.json())
     .then((responseJson) => {
-      return responseJson;
+      console.log(`response = ${JSON.stringify(responseJson)}`)
+      this.setState({ materias_data: responseJson });
     })
     .catch((error) => {
       console.error(error);
     });
+  }
 }
-
-const styles = StyleSheet.create({
-  container: { flex: 1, padding: 16, paddingTop: 30, backgroundColor: '#fff' },
-  head: { height: 40, backgroundColor: '#f1f8ff' },
-  text: { margin: 6 },
-  textContainer: {
-    margin: 20
-  },
-  btn: {
-    padding: 15,
-    width: "66%",
-    margin: 70,
-    borderWidth: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#841584',
-  },
-  txt: {
-    color: '#C0C0C0',
-  },
-})
