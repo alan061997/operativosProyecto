961bc7a46b29a0dc3e30c8ca845b40917639eb5e
alan061997 <alan061997@gmail.com>
2018-04-21 15:49:13 -0500

    muestra cursos con info de la vista inscripcion

--- Screens/CursoScreen.js
+++ Screens/CursoScreen.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Alert, AppRegistry, TouchableOpacity, StyleSheet, Text, View } from 'react-native';
+import { Alert, AppRegistry, TouchableOpacity, StyleSheet, ScrollView, Text, View } from 'react-native';
+import { Table, TableWrapper, Row, Rows, Cell } from 'react-native-table-component';
 import {StackNavigator} from 'react-navigation';
 
 
@@ -16,21 +17,85 @@ export default class CursoScreen extends Component {
     this.state = {
       user_data: this.props.navigation.state.params.user_data,
       student_data: this.props.navigation.state.params.student_data,
+      semestre: this.props.navigation.state.params.semestre,
+      materia: this.props.navigation.state.params.materia,
+      tableHead: ['Grupo', 'Maestro', 'Frecuencia', 'Hora Inicio', 'Hora Fin'],
+      tableData: [
+        ['1.1', '1.2', '1.3', '1.4', '1.5'],
+        ['2.1', '2.2', '2.3', '2.4', '2.5'],
+    ]
     };
+  }
+  componentDidMount(){
+    this.getCursos().done();
+  }
+
+  showData(cell_index, row_index){
+    Alert.alert(this.state.tableData[row_index][0].toString());
   }
   render() {
     const { navigate } = this.props.navigation;
     const { params } = this.props.navigation.state;
-    return (
-      <View style={styles.container}>
-        <Text>Vista de Curso</Text>
-        <View style={styles.container}>
-          <TouchableOpacity onPress={() => this.props.navigation.goBack()} style={styles.btn}>
-            <Text style={styles.txt}>Go Back</Text>
-          </TouchableOpacity>
+    const state = this.state;
+    const cell_button = (data, cell_index, row_index) => (
+      <TouchableOpacity style={styles.cell_btn} onPress={() => this.showData(cell_index, row_index)}>
+        <View>
+          <Text style={styles.txt}>{data}</Text>
         </View>
-      </View>
+      </TouchableOpacity>
     );
+    return (
+      <ScrollView>
+        <View style={styles.container}>
+          <Text>Vista de Curso</Text>
+          <Text>Semestre: {this.state.semestre}</Text>
+          <Text>Materia: {this.state.materia}</Text>
+          <View style={styles.container}>
+            <Table borderStyle={{ borderWidth: 2, borderColor: '#841584'}}>
+              <Row data={state.tableHead} flexArr={[2, 2, 2, 2 ,2]} style={styles.head} textStyle={styles.txt}/>
+              {
+                state.tableData.map((rowData, row_index) => (
+                  <TableWrapper key={row_index} flexArr={[2, 2, 2, 2, 2]} style={styles.row}>
+                    {
+                      rowData.map((cellData, cell_index) => (
+                        <Cell key={cell_index} data={cell_index === 0 ? cell_button(cellData, cell_index, row_index) : cellData} textStyle={styles.txt} />
+                      ))
+                    }
+                  </TableWrapper>
+                ))
+              }
+            </Table>
+          </View>
+        </View>
+      </ScrollView>
+    );
+  }
+  getCursos = async() => {
+    fetch('http://sis-operativos-2018.herokuapp.com/Backend/getCursos.php', {
+      method: 'POST',
+      headers: {'Accept' : "application/json", 'Content-Type' : 'application/json',},
+      body: JSON.stringify({
+            materia: this.state.materia,
+        })
+    })
+    .then((response) => response.json())
+    .then((res) => {
+      console.log(`response = ${JSON.stringify(res)}`)
+      if (res.length > 0){
+        tableData = [];
+        for (i = 0; i < res.length; i++){
+          row = [res[i].grupo, res[i].nombre, res[i].diaSemana, res[i].horaInicio, res[i].horaFin];
+          tableData.push(row);
+        }
+        this.setState({tableData: tableData});
+      }
+      else{
+        console.log("Cursos not found");
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+    });
   }
 }
 
@@ -55,5 +120,17 @@ const styles = StyleSheet.create({
   },
   txt: {
     color: '#C0C0C0',
+  },
+  head: { height: 40, backgroundColor: '#841584' },
+  text: { margin: 6 },
+  row: { flexDirection: 'row', backgroundColor: '#FFF1C1' },
+  cell_btn: {
+    padding: 5,
+    width: "66%",
+    margin: 10,
+    borderWidth: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#841584',
   },
 })
